01fdf2fc51275384dbb8e4d7d7fa87ac4a80d4f6
Yogesh-chouhan-hub <yogeshchouhan2253@gmail.com>
2026-05-14 12:45:51 +0530

    final portfolio deploy

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
-
+const mongoose = require("mongoose");
+const Contact = require("./models/contactSchema");
+require("dotenv").config();
 const PORT = process.env.PORT || 8888;
-
-/* ======================================================
-                    MIDDLEWARE
-====================================================== */
 
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
-/* ======================================================
-                    VIEW ENGINE
-====================================================== */
-
 app.set("view engine", "ejs");
 
 app.set("views", path.join(__dirname, "views"));
 
-/* ======================================================
-                    STATIC FILES
-====================================================== */
-
 app.use(express.static(path.join(__dirname, "public")));
 
-/* ======================================================
-                    ROUTES
-====================================================== */
+main()
+  .then(() => {
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-/* HOME */
+async function main() {
+  await mongoose.connect(process.env.MONGO_URL);
+}
 
 app.get("/", (req, res) => {
   res.redirect("/yogesh");
@@ -42,28 +37,24 @@ app.get("/yogesh", (req, res) => {
 });
 
 /* CONTACT FORM */
-
-app.post("/contact", (req, res) => {
+app.post("/contact", async (req, res) => {
   const { name, email, message } = req.body;
-
-  console.log(name);
-  console.log(email);
-  console.log(message);
-
-  res.send("Message Sent Successfully");
+  try {
+    let newMsg = new Contact({
+      name: name,
+      email: email,
+      message: message,
+    });
+    await newMsg.save();
+    res.redirect("/yogesh");
+  } catch (err) {
+    console.log(err);
+  }
 });
-
-/* ======================================================
-                    404 PAGE
-====================================================== */
 
 app.use((req, res) => {
   res.status(404).send("Page Not Found");
 });
-
-/* ======================================================
-                    SERVER
-====================================================== */
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
